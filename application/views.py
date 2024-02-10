c3fb6d828f5f5e2579249a0972b7223ce63b1019
Jessica Delorme-Po <jessicadelormepo@cmail.carleton.ca>
2024-02-10 12:57:29 -0500

    Fixing default django code to pass pylint (Test 1)

--- application/views.py
+++ application/views.py
@@ -1,9 +1,11 @@
+"""Module for application views"""
 from django.shortcuts import render
 
 
 def index(request):
+    """View for index page (AKA homepage)"""
+
     # Copied from Lab 2 (NEEDS TO BE CHANGED)
     posts = [{"author": "Jimmy", "contents": "I am the coolest!"},
-                  {"author": "Samiha", "contents": "I am even cooler!"}]
+             {"author": "Samiha", "contents": "I am even cooler!"}]
     return render(request, 'index.html', {"blog_posts": posts})
-
