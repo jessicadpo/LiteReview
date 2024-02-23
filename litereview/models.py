8c49d5fe5c9c63a73df11ae55b345582faae1aee
unknown <pgmossman@gmail.com>
2024-02-23 14:29:01 -0500

    User model commented, with suggestions for changes

--- litereview/models.py
+++ litereview/models.py
@@ -5,10 +5,15 @@ from django.db import models
 class User(models.Model):
     """Class for User database table"""
     usr_id = models.IntegerField(primary_key=True)
+    '''unique user id (auto-generated? look into)'''
     usr_name = models.CharField(max_length=64)
+    '''user defined username'''
     full_name = models.CharField(max_length=64)
+    '''user's full real name (discuss: split into first & last or remove entirely? why do we need this)'''
     password = models.CharField(max_length=64)
+    '''user's password (hidden/encrypted? look into)'''
     email = models.CharField(max_length=64)
+    '''user's email (can be set as email field to allow validation, but needs infrastructure; discuss)'''
 
 
 
