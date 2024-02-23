5174f501f5468912b3ed83d3fd13bffeca31d619
unknown <pgmossman@gmail.com>
2024-02-23 14:24:00 -0500

    fields given typing

--- litereview/models.py
+++ litereview/models.py
@@ -5,7 +5,12 @@ from django.db import models
 class User(models.Model):
     """Class for User database table"""
     usr_id = models.IntegerField(primary_key=True)
-    usr_name =
-    full_name =
-    password =
-    email = 
+    usr_name = models.CharField(max_length=64)
+    full_name = models.CharField(max_length=64)
+    password = models.CharField(max_length=64)
+    email = models.CharField(max_length=64)
+
+
+
+
+
