4ac987c5f808648da38fc5686ffcd9c8b0893122
Jessica Delorme-Po <jessicadelormepo@cmail.carleton.ca>
2024-02-24 12:42:47 -0500

    Added temp URL to be able to review with Django & deleted pycache folder to prevent future merge conflicts

--- litereview/urls.py
+++ litereview/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import index
+from .views import index, temp_review_modal
 
 urlpatterns = [
     path('', index),
+    path('temp_review', temp_review_modal)
 ]
--- litereview/views.py
+++ litereview/views.py
@@ -9,3 +9,13 @@ def index(request):
     posts = [{"author": "Jimmy", "contents": "I am the coolest!"},
              {"author": "Samiha", "contents": "I am even cooler!"}]
     return render(request, 'index.html', {"blog_posts": posts})
+
+
+def temp_review_modal(request):
+    """Temporary view for rendering Create Review modal form"""
+    return render(request, 'StatReview.html')
+
+
+
+
+
