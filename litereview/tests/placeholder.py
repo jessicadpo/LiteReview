dd97dd90a486e2011321fe2de5b9bbb7a73f9842
Jessica Delorme-Po <jessicadelormepo@cmail.carleton.ca>
2024-02-24 11:40:05 -0500

    Added macOS instructions & Prepared template for Deliverable 2 README requirements

---
+++ litereview/tests/placeholder.py
@@ -0,0 +1,6 @@
+"""Placeholder module so that tests folder properly commits to remote repository"""
+
+
+class PlaceHolder:
+    """Placeholder class"""
+    print('This is to get pylint to shut up about the file being empty. Replace this entire file in Sprint #2')
