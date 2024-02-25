b600c29e339d775497a2ea5cebc95acae8452e62
Thomas <thomasmurphy4@cmail.carleton.ca>
2024-02-25 12:48:56 -0500

    To be complient with linter now

--- litereview/views.py
+++ litereview/views.py
@@ -1,7 +1,6 @@
 """Module for litereview views"""
 from django.shortcuts import render
 
-
 def index(request):
     """View for index page (AKA homepage)"""
 
@@ -10,12 +9,10 @@ def index(request):
              {"author": "Samiha", "contents": "I am even cooler!"}]
     return render(request, 'index.html', {"blog_posts": posts})
 
-
 def userpage(request):
+    """View for userpage"""
     return render(request, 'userpage.html')
-
 
 def temp_review_modal(request):
     """Temporary view for rendering Create Review modal form"""
     return render(request, 'StatReview.html')
-
